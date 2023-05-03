6f4b2cb64249739cb72f98f879ffed2b3b9569e5
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-05-03 23:37:39 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Extension from './plugin'
+import { Scratch } from './vm'
 
 new Extension({
   id: 'openai',
@@ -11,10 +12,32 @@ new Extension({
       text: '询问 ChatGPT [message:string]',
       default: { message: '' },
       bind: async ({ message }) => {
-        console.log(message);
-        return message;
+        try {
+          const resp = await fetch(`https://chatgpt.deeptrain.net/gpt`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ data: message })
+          })
+          const data = await resp.json();
+          return data.message;
+        } catch (e) {
+          console.error(e);
+          return "请求过多！请稍后重试！"
+        }
       },
       disableMonitor: true,
+    },
+    {
+      opcode: 'donate', //@ts-ignore
+      blockType: Scratch.BlockType.COMMAND,
+      text: "捐赠",
+      bind: () => window.open("https://afdian.net/@zmh-program"),
+    },
+    {
+      opcode: 'info', //@ts-ignore
+      blockType: Scratch.BlockType.COMMAND,
+      text: "(使用过程中有非正常言论请忽略)",
+      bind: () => "",
     }
   ]
 }).register()
