dc04943111b18bf5941e215b31772d333a1b4a21
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-05-03 22:37:51 +0800

    initialize

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import Extension from './plugin'
 
 new Extension({
-  id: '',
-  name: '',
-  color1: '#0800ff',
-  blocks: []
+  id: 'openai',
+  name: 'ChatGPT',
+  color1: '#0e0e0e',
+  blocks: [
+    {
+      opcode: 'ask', /** @ts-ignore */
+      blockType: Scratch.BlockType.REPORTER,
+      text: '询问 ChatGPT [message:string]',
+      default: { message: '' },
+      bind: async ({ message }) => {
+        console.log(message);
+        return message;
+      },
+      disableMonitor: true,
+    }
+  ]
 }).register()
